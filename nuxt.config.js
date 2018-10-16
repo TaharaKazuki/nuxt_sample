856cf067a70235ddbb669c8406bf56bc41c57e98
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2018-10-17 00:17:58 +0900

    [add] sample file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 const pkg = require('./package')
+const path = require('path')
 
 module.exports = {
   mode: 'universal',
@@ -47,6 +48,17 @@ module.exports = {
   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
+  },
+  /*
+  ** editor jump
+  */
+  resolve: {
+    extensions: ['.js', '.json', '.vue', '.ts'],
+    root: path.resolve(__dirname),
+    alias: {
+      '@': path.resolve(__dirname),
+      '~': path.resolve(__dirname),
+    },
   },
 
   /*
---
+++ pages/users.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" v-model="userId">
+    <button @click="movePath">Click</button>
+    <nuxt-child />
+  </div>
+</template>
+<script>
+
+  export default {
+    data() {
+      return {
+        userId: ''
+      }
+    },
+    methods: {
+      movePath() {
+        this.$router.push(`/users/${this.userId}`)
+      }
+    }
+  }
+
+</script>
